5d6a31e4655a91b30d49efc924acee2708226927
Samy Celis <33494707+ureshiichan@users.noreply.github.com>
2022-03-02 11:43:03 -0600

    update

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "holiiiis, prueba de deploy");
+		get("/hello", (req, res) -> "holiiiis, prueba de deploy y webhook");
 	}
 }
